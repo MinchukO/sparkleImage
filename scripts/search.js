f3fd436ff374d611a0fdf98ae3a579d57d7dbef8
Minchuk Olga <olha.minchuk@mgroupweb.com>
2022-06-17 07:00:27 +0200

    changed mobile menu

--- scripts/search.js
+++ scripts/search.js
@@ -1,4 +1,4 @@
-const btn = document.querySelector(".header__top__tablets__search");
+const btn = document.querySelector(".header__top__right-menu__tablets-search");
 const form = document.querySelector(".header__top__right-menu__search");
 
 btn.addEventListener('click', (e) => {
